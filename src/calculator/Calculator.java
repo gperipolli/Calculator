92bd1b2d424772769e3d3958ff51dccc1957f975
gperipolli <gperipolli@inf.ufsm.br>
2015-10-01 18:07:01 -0300

    Arrays containing operators and numbers

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -5,12 +5,17 @@
  */
 package calculator;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author gperipolli
  */
 public class Calculator {
     private static View calc;
+    private static ArrayList<Double> numbers;
+    private static ArrayList<Operator> operators;
+    
 
     /**
      * @param args the command line arguments
@@ -33,9 +38,8 @@ public class Calculator {
         calc.set_text(text.substring(0, text.length()-1));
     }
 
-
     protected static void ButtonEqual() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        calc.set_text(compute_expression());
     }
     private static String get_text ()
     {
@@ -44,6 +48,54 @@ public class Calculator {
     private static void set_text(String text)
     {
         calc.set_text(text);
+    }
+
+    private static String compute_expression() {
+        get_numbers_and_operators ();
+        operate();
+        return numbers.get(0).toString();
+    }
+    //terminar metodos de calculo
+    private static void get_numbers_and_operators() {
+        String expression = calc.get_text();
+        String number ="";
+        Character c;
+        for (int i=0; i<expression.length();i++)
+        {
+            c = expression.charAt(i);
+            if (is_square(c)){
+                addOperator(c);
+                i++;
+                numbers.add(Double.valueOf(number));
+                number="";
+            }else{
+                if (is_operator(c))
+                {
+                    addOperator(c);
+                    numbers.add(Double.valueOf(number));
+                    number="";
+                }else
+                {
+                    number+=c;
+                }
+        }
+        }
+    }
+
+    private static void operate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static boolean is_operator(char c) {
+        return c == '+' || c == '-' || c == 'x' || c == '/'||c == '%';
+    }
+
+    private static boolean is_square(char c) {
+        return c == '^';      
+    }
+
+    private static void addOperator(Character c) {
+        
     }
 
     
